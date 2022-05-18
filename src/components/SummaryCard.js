aff158348f09b05ca60c591fb027088f234cf045
Junrong <87742302+akiraafu@users.noreply.github.com>
2022-05-18 21:07:31 +0800

    Update SummaryCard.js

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -3,14 +3,14 @@ import moment from "moment";
 function SummaryCard({ day }) {
     let day_icon = `${process.env.REACT_APP_ICON_URL + day.weather[0]["icon"]}@2x.png`;
     return (
-        <li className='container p-4 flex items-center justify-center bg-gray-200 rounded-lg my-auto mr-1'>
+        <li className='p-4 flex items-center justify-center bg-gray-200 rounded-lg my-auto mr-1 xsm:mr-0 '>
             <div className='my-auto'>
-                <p className='font-bold text-3xl text-pink-600 mb-2'>
+                <p className='font-bold text-3xl text-yellow-400 mb-2 xsm:text-3xl'>
                     {Math.round(day.main.temp)}&deg;C
                 </p>
-                <p className='text-2xl text-gray-800 tracking-widest'>
+                <p className='text-2xl text-gray-800 tracking-widest xsm:text-xl'>
                     {day.weather[0].main}
-                    <img src={day_icon} className='w-1/4 inline' />
+                    <img src={day_icon} className='w-1/4 inline' alt='icon' />
                 </p>
                 <p className='text-gray-400 text-xs uppercase tracking-widest'>
                     {day.weather[0].description}
