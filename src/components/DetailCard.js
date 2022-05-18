473ea6d8632a1c8a37b4ac503656e439b84931f4
Junrong <87742302+akiraafu@users.noreply.github.com>
2022-05-18 21:06:33 +0800

    Update DetailCard.js

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -3,22 +3,22 @@ import moment from "moment";
 function DetailCard({ weather_icon, data }) {
     const { clouds, main, weather } = data.list[0];
     return (
-        <div className='conatainer p-4 flex items-center justify-center shadow-lg rounded-lg bg-white h-1/3 mb-auto'>
-            <div className='my-auto'>
-                <p className='font-bold text-5xl text-pink-800 mb-2'>
+        <div className='conatainer p-4 flex items-center justify-center shadow-lg rounded-lg bg-white h-1/3 mb-auto xsm:flex-col'>
+            <div className='my-auto w-2/4 xsm:w-11/12'>
+                <p className='font-bold text-5xl text-yellow-500 mb-2 xsm:text-3xl'>
                     {Math.round(main.temp)}&deg;C
                 </p>
-                <p className='text-4xl text-gray-800 tracking-widest'>
+                <p className='text-4xl text-gray-800 tracking-widest xsm:text-2xl'>
                     {weather[0].main}
-                    <img src={weather_icon} className='w-1/4 inline' />
+                    <img src={weather_icon} className='w-1/4 inline' alt='icon' />
                 </p>
                 <p className='text-gray-400 text-xs uppercase tracking-widest'>
                     {weather[0].description}
                 </p>
                 <p className='tracking-wider'>{moment().format("dddd MMM YYYY")}</p>
             </div>
-            <div className='my-2 border-l-2 border-gray-100'>
-                <p className='text-gray-400 text-lg'>
+            <div className='my-2 border-l-2 border-gray-100 xsm:border-0 xsm:w-11/12 '>
+                <p className='text-gray-400 text-lg '>
                     RealFeel: {Math.round(main.feels_like)}&deg;C
                 </p>
                 <p className='text-gray-400 text-lg'>Humidity: {main.humidity}%</p>
